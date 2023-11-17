5c3dd1b643a4be817fba6d18a9056af8b7407646
Kiss Tamás <kiss.tamas.j@gmail.com>
2023-11-17 15:49:39 +0100

    feat: axios server side

--- frontend/app/(admin-interface)/admin/(root)/(routes)/users/page.tsx
+++ frontend/app/(admin-interface)/admin/(root)/(routes)/users/page.tsx
@@ -1,36 +1,28 @@
-import options from "@/app/api/auth/[...nextauth]/options";
 import H1 from "@/components/ui/H1";
 import PageContainer from "@/components/ui/PageContainer";
 import PageHeaderContainer from "@/components/ui/PageHeaderContainer";
 import PageHeaderSeparator from "@/components/ui/PageHeaderSeparator";
-import { BACKEND_URL } from "@/lib/constants";
+import { axiosServer } from "@/lib/axiosServer";
 import { User } from "@/lib/types/user";
-import { getServerSession } from "next-auth";
 
 const getUser = async (): Promise<User> => {
-  console.log('getUser requrest')
-  const session = await getServerSession(options);
-  const response = await fetch(BACKEND_URL +`/users/profile`, {
-    method: "GET",
-    headers: {
-      authorization: `Bearer ${session?.backendTokens.accessToken}`,
-      "Content-Type": "application/json",
-    },
-  })
+  console.log("getUser requrest");
+  const axios = await axiosServer();
+  const { data } = await axios.get(`/users/profile`);
 
-  return await response.json()
-}
+  return data
+};
 
 export default async function AdminHomePage() {
-  const profile = await getUser()
-  console.log({profile})
+  const profile = await getUser();
+  console.log({ profile });
   return (
     <PageContainer>
       <PageHeaderContainer>
         <H1>Felhasználók</H1>
       </PageHeaderContainer>
       <PageHeaderSeparator />
-      <div>{ profile ? profile.email : 'Felhasználók oldal oldal' }</div>
+      <div>{profile ? profile.email : "Felhasználók oldal oldal"}</div>
     </PageContainer>
   );
 }
