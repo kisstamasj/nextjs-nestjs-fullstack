65bb057ea6668c9e535d5478da272b5c9ee08c6e
Kiss Tamás <kiss.tamas.j@gmail.com>
2024-01-06 14:44:53 +0100

    asd

--- frontend/components/PageContainer.tsx
+++ frontend/components/PageContainer.tsx
@@ -8,8 +8,12 @@ interface PageContainerProps {
 
 const PageContainer: FC<PageContainerProps> = ({ children }) => {
   return (
-    <div className={`md:p-3 p-1 bg-gray-100 dark:bg-slate-950 h-full animate-fade`}>
-      <div className="bg-white md:p-8 p-4 dark:bg-slate-900 rounded-sm">{children}</div>
+    <div
+      className={`md:p-3 p-1 bg-gray-100 dark:bg-slate-950 h-full animate-fade z-10`}
+    >
+      <div className="bg-white md:p-8 p-4 dark:bg-slate-900 rounded-sm">
+        {children}
+      </div>
     </div>
   );
 };
