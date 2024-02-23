4e35550628316bdb5c5fc40c88e3724d075b989c
Kiss Tamás <kiss.tamas.j@gmail.com>
2024-02-23 09:46:34 +0100

    format

--- frontend/components/menu/MenuItem.tsx
+++ frontend/components/menu/MenuItem.tsx
@@ -31,16 +31,32 @@ const MenuItem: FC<MenuItemProps> = ({ item }) => {
     if (isMobileScreen()) {
       sidebar.toggleSidebar();
     }
-  }
+  };
 
   return (
     <>
-      <div className={cn("w-full", isActive ? "bg-gray-200 dark:bg-slate-800" : "")}>
+      <div
+        className={cn(
+          "w-full",
+          isActive ? "bg-gray-200 dark:bg-slate-800" : ""
+        )}
+      >
         <Link href={item.href} className={linkClass} onClick={menuItemClick}>
           <div className="">
-            <Icon name={item.icon} className={cn("text-gray-500 dark:text-white")} size={20} />
+            <Icon
+              name={item.icon}
+              className={cn("text-gray-500 dark:text-white")}
+              size={20}
+            />
           </div>
-          <div className={cn(`transition-all duration-500 ease-linear`, sidebar.status === "mini" ? "hidden" : "block")}>{item.label}</div>
+          <div
+            className={cn(
+              `transition-all duration-500 ease-linear`,
+              sidebar.status === "mini" ? "hidden" : "block"
+            )}
+          >
+            {item.label}
+          </div>
         </Link>
       </div>
     </>
