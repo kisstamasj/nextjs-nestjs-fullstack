66cda0c9ae2c4d048f6ce0b62615251c1adec464
Kiss Tamás <kiss.tamas.j@gmail.com>
2024-02-29 23:29:40 +0100

    fix: disable caching

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const revalidate = 0;
+
 export const metadata: Metadata = {
   title: "Learningage v2",
   description: "Learningage v2",
