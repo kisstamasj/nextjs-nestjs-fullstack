603dae399c349ad4b50eb6ae90ba1c772006f03e
Kiss Tamás <kiss.tamas.j@gmail.com>
2024-03-02 00:44:39 +0100

    refact: page loader without PageContainer

--- frontend/app/(admin-interface)/admin/loading.tsx
+++ frontend/app/(admin-interface)/admin/loading.tsx
@@ -4,11 +4,9 @@ import { FC } from "react";
 
 const Loading: FC = () => {
   return (
-    <PageContainer>
-      <div className="w-full h-full flex items-center justify-center">
-        <Loader />
-      </div>
-    </PageContainer>
+    <div className="w-full h-full flex items-center justify-center">
+      <Loader />
+    </div>
   );
 };
 
