0697daa57166dea7b0766c7005df98b9938d9075
Kiss Tamás <kiss.tamas.j@gmail.com>
2024-03-03 14:57:45 +0100

    refact: side bar overlay with the alert dialog overlay

--- frontend/components/sidebar/overlay.tsx
+++ frontend/components/sidebar/overlay.tsx
@@ -1,20 +1,16 @@
 "use client";
 
 import { useSidebar } from "@/hooks/use-sidebar";
-import { cn } from "@/lib/utils";
 import { FC } from "react";
+import { AlertDialog, AlertDialogOverlay } from "../ui/alert-dialog";
 
 const Overlay: FC = ({}) => {
   const sidebar = useSidebar();
   return (
     <>
-      {sidebar.status === "open" && (
-        <div
-          className={cn(
-            "fixed left-0 top-0 z-40 h-full w-full bg-black/80 lg:hidden backdrop-blur-sm animate-fade"
-          )}
-        />
-      )}
+      <AlertDialog open={sidebar.status === "open"}>
+        <AlertDialogOverlay />
+      </AlertDialog>
     </>
   );
 };
